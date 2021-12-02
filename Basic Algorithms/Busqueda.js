1eaabc44dda97dc7fca4fe7755a1a14522d88855
JohnDan72 <mr._dany@hotmail.com>
2021-12-02 16:12:28 -0600

    Avance en algoritmos de búsqueda

--- Basic Algorithms/Busqueda.js
+++ Basic Algorithms/Busqueda.js
@@ -1,9 +1,31 @@
 
-// // binaria
-// const binarySearch = ( busca = null , arr = [] ) => {
-//     let left = 0 , right = arr.length - 1;
-//     let middle = Math.trunc(arr.length/2);
-//     while( left < right ){
+// binaria
+const binarySearch = (busca = null, data = []) => {
+    let left = 0, right = data.length - 1;
 
-//     }   
-// }
+    let response = {
+        ok: false,
+        errors: [{ msg: 'El elemento no se encuentra' }]
+    };
+
+    while (left <= right) {
+        const middle = Math.trunc((left + right) / 2);
+        if (data[middle] == busca) {
+            response = {
+                ok: true,
+                index: middle
+            }
+            break;
+        }
+        if (data[middle] < busca) left = middle + 1;
+        else right = middle - 1;
+    }
+
+    return response;
+}
+const data = [];
+for (let i = 0; i < 100; i++) {
+    data.push(i + 1);
+}
+const busca = 887;
+console.log(binarySearch(busca, data));
